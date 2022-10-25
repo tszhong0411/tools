713c2f1bbb432a73c6230ddf9478149628abc8d0
tszhong0411 <lainelson411@gmail.com>
2022-10-25 21:05:07 +0800

    :recycle: Use axios instead of fetch

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -1,15 +1,13 @@
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const url = req.query.url as string
-
-  const response = await fetch(url)
-  const blob = await response.blob()
-  const arrayBuffer = await blob.arrayBuffer()
-  const buffer = Buffer.from(arrayBuffer)
+  const { data: buffer } = await axios.get(url, {
+    responseType: 'arraybuffer',
+  })
 
   res.setHeader('Content-Type', 'application/pdf')
 
