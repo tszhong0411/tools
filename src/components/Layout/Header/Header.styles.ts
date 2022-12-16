4b925a60cf705a5f81aa0ae8f1ebaa2bcc5cf9ff
tszhong0411 <lainelson411@gmail.com>
2022-12-16 22:32:02 +0800

    :sparkles: update header

--- src/components/Layout/Header/Header.styles.ts
+++ src/components/Layout/Header/Header.styles.ts
@@ -8,76 +8,6 @@ export const useStyles = createStyles((theme) => ({
     height: 56,
   },
 
-  links: {
-    display: 'none',
-
-    [theme.fn.largerThan('sm')]: {
-      display: 'flex',
-    },
-  },
-
-  burger: {
-    marginRight: theme.spacing.md,
-
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  link: {
-    display: 'block',
-    lineHeight: 1,
-    padding: '8px 12px',
-    borderRadius: theme.radius.sm,
-    textDecoration: 'none',
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({
-        variant: 'light',
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-        .color,
-    },
-  },
-
-  logo: {
-    height: 34,
-
-    '& svg': {
-      fill: theme.colors.red[9],
-      height: 34,
-      '& > path:last-child': {
-        fill: theme.colorScheme === 'dark' ? '#fff' : '#000',
-      },
-      cursor: 'pointer',
-      padding: '6px 8px',
-      borderRadius: 8,
-
-      '&:hover': {
-        backgroundColor:
-          theme.colorScheme === 'dark'
-            ? theme.colors.dark[8]
-            : theme.colors.gray[0],
-      },
-    },
-  },
-
   button: {
     width: 34,
     height: 34,
@@ -88,32 +18,5 @@ export const useStyles = createStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    color: theme.colorScheme === 'dark' ? theme.white : theme.colors.gray[7],
-    backgroundColor:
-      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.white,
-
-    '&:hover': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[3]
-          : theme.colors.gray[0],
-    },
-  },
-
-  mobileNavbar: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: -10,
-    height: `100vh`,
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
-    borderTopWidth: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
   },
 }))
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,51 +1,23 @@
 import {
-  Burger,
   Button,
   Container,
   Group,
   Header as MantineHeader,
-  Paper,
   Tooltip,
-  Transition,
   useMantineColorScheme,
 } from '@mantine/core'
-import { useDisclosure, useScrollLock } from '@mantine/hooks'
 import { IconBrandGithub, IconMoonStars, IconSun } from '@tabler/icons'
-import { useRouter } from 'next/router'
 import React from 'react'
 
 import Link from '@/components/Link'
 
 import { useStyles } from './Header.styles'
-import { Links } from './Links'
 import { Logo } from './Logo'
 
 export default function Header() {
-  const [opened, { toggle }] = useDisclosure(false)
   const { colorScheme, toggleColorScheme } = useMantineColorScheme()
-  const [_, setScrollLocked] = useScrollLock()
-  const { classes, cx } = useStyles()
-  const router = useRouter()
+  const { classes } = useStyles()
   const dark = colorScheme === 'dark'
-
-  const toggleNavbar = () => {
-    toggle()
-    setScrollLocked((c) => !c)
-  }
-
-  const items = Links.map((link) => (
-    <Link
-      key={link.label}
-      href={link.url}
-      className={cx(classes.link, {
-        [classes.linkActive]: router.asPath === link.url,
-      })}
-      onClick={() => opened && toggleNavbar()}
-      underline={false}
-    >
-      {link.label}
-    </Link>
-  ))
 
   return (
     <>
@@ -56,22 +28,13 @@ export default function Header() {
         }}
       >
         <Container className={classes.inner}>
-          <Burger
-            opened={opened}
-            onClick={() => toggleNavbar()}
-            size='sm'
-            className={classes.burger}
-          />
-          <Group className={classes.links} spacing={5}>
-            {items}
-          </Group>
-          <Link href='/' className={classes.logo}>
+          <Link href='/' variant='text' color={dark ? '#fff' : '#000'}>
             <Logo />
           </Link>
           <Group spacing={5}>
             <Tooltip label={dark ? 'Light mode' : 'Dark mode'} openDelay={500}>
               <Button
-                variant='filled'
+                variant='light'
                 color='gray'
                 className={classes.button}
                 onClick={() => toggleColorScheme()}
@@ -81,23 +44,16 @@ export default function Header() {
             </Tooltip>
             <Tooltip label='Source code' openDelay={500}>
               <Button
-                variant='filled'
+                variant='light'
                 color='gray'
                 className={classes.button}
                 component={Link}
-                href='https://github.com/tszhong0411/tools.honghong.me'
+                href='https://github.com/tszhong0411/tools'
               >
                 <IconBrandGithub size={18} />
               </Button>
             </Tooltip>
           </Group>
-          <Transition transition='slide-right' duration={200} mounted={opened}>
-            {(styles) => (
-              <Paper className={classes.mobileNavbar} style={styles}>
-                {items}
-              </Paper>
-            )}
-          </Transition>
         </Container>
       </MantineHeader>
     </>
