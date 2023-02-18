d5788eafd50b1bb5e676bf430bc756cefca6081a
tszhong0411 <lainelson411@gmail.com>
2023-02-18 19:57:18 +0800

    fix: 修復 Typescript 錯誤

--- src/lib/svg-to-base64.ts
+++ src/lib/svg-to-base64.ts
@@ -2,37 +2,39 @@ export const svgToBase64 = (file: Blob, callback: (result: string) => void) => {
   const reader = new FileReader()
 
   reader.addEventListener('load', (e) => {
-    const svgURL = URL.createObjectURL(
-      new Blob([e.target.result], {
-        type: 'image/svg+xml',
+    if (e.target && e.target.result) {
+      const svgURL = URL.createObjectURL(
+        new Blob([e.target.result], {
+          type: 'image/svg+xml',
+        })
+      )
+
+      const svgImage = document.createElement('img')
+
+      // Set the style to prevent it from appearing in the body
+      // but make sure it has height and width
+      svgImage.style.position = 'absolute'
+      svgImage.style.opacity = '0'
+      svgImage.style.visibility = 'hidden'
+
+      document.body.appendChild(svgImage)
+
+      svgImage.addEventListener('load', () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = svgImage.clientWidth
+        canvas.height = svgImage.clientHeight
+        canvas.getContext('2d')?.drawImage(svgImage, 0, 0)
+
+        const dataURL = canvas.toDataURL('image/png')
+
+        callback(dataURL)
+
+        document.body.removeChild(svgImage)
       })
-    )
 
-    const svgImage = document.createElement('img')
+      svgImage.src = svgURL
 
-    // Set the style to prevent it from appearing in the body
-    // but make sure it has height and width
-    svgImage.style.position = 'absolute'
-    svgImage.style.opacity = '0'
-    svgImage.style.visibility = 'hidden'
-
-    document.body.appendChild(svgImage)
-
-    svgImage.addEventListener('load', () => {
-      const canvas = document.createElement('canvas')
-      canvas.width = svgImage.clientWidth
-      canvas.height = svgImage.clientHeight
-      canvas.getContext('2d').drawImage(svgImage, 0, 0)
-
-      const dataURL = canvas.toDataURL('image/png')
-
-      callback(dataURL)
-
-      document.body.removeChild(svgImage)
-    })
-
-    svgImage.src = svgURL
+      reader.readAsText(file)
+    }
   })
-
-  reader.readAsText(file)
 }
