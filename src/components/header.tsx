a7f70de6c62581add86399aea0e5427715612f6f
tszhong0411 <lainelson411@gmail.com>
2023-08-27 00:53:23 +0800

    feat: improve accessibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className='fixed left-0 right-0 top-0 z-40 bg-black/50 shadow-sm saturate-100 backdrop-blur-[10px]'>
       <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
-        <Link href='/'>
+        <Link href='/' aria-label='Home' title='Home'>
           <Logo />
         </Link>
         <div className='flex items-center justify-center gap-6'>
@@ -16,6 +16,8 @@ const Header = () => {
             href='https://github.com/tszhong0411/tools'
             target='_blank'
             rel='noreferrer noopener'
+            aria-label='GitHub'
+            title='GitHub'
           >
             <GithubIcon />
           </a>
