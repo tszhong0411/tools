286c138faca05e17815118a37d6c320af0dc4a6e
tszhong0411 <me@honghong.me>
2025-01-25 23:47:38 +0800

    feat: update hero component text and gradient colors for improved clarity

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,17 +6,18 @@ const Hero = () => {
     <div className='my-12 space-y-8'>
       <h1 className='text-3xl font-extrabold'>
         The{' '}
-        <span className='bg-gradient-to-r from-red-600 to-orange-500 bg-clip-text text-transparent'>
+        <span className='bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent'>
           Best
         </span>{' '}
         Online Tools
       </h1>
       <p className='leading-6 text-muted-foreground'>
-        Here are some free online tools made by{' '}
+        Discover a powerful collection of web tools designed to streamline your workflow and boost
+        productivity. Made by{' '}
         <Link href='https://instagram.com/tszhong0411' className='text-white'>
           @tszhong0411
         </Link>
-        . I hope to train my skills. This may be my side project :)
+        . I hope to practice my skills.
       </p>
       <div className='flex gap-4'>
         <Link href='#get-started' className={buttonVariants()}>
