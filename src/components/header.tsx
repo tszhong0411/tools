88d02642f0ee9baee0de3aa327af6e53249e6263
tszhong0411 <me@honghong.me>
2025-01-26 17:47:19 +0800

    refactor: streamline GitHub link in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,16 +3,14 @@ import { Link, Logo } from '@tszhong0411/ui'
 
 const Header = () => {
   return (
-    <header className='fixed inset-x-0 top-0 z-40 shadow-sm saturate-100 backdrop-blur-[10px]'>
+    <header className='fixed inset-x-0 top-0 z-40 saturate-100 backdrop-blur-[10px]'>
       <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
         <Link href='/' aria-label='Home'>
           <Logo width={24} height={24} />
         </Link>
-        <div className='flex items-center justify-center gap-6'>
-          <Link href='https://github.com/tszhong0411/tools' aria-label='GitHub'>
-            <SiGithub />
-          </Link>
-        </div>
+        <Link href='https://github.com/tszhong0411/tools' aria-label='GitHub'>
+          <SiGithub />
+        </Link>
       </div>
     </header>
   )
