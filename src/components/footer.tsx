0cffeb124d2e7c0a5dd3ee45f6f6b9f5dd904e5e
tszhong0411 <me@honghong.me>
2025-02-04 01:01:55 +0800

    fix: update name in footer and metadata

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='mx-auto flex max-w-4xl flex-col px-8 pb-8 text-sm'>
-      &copy; {new Date().getFullYear()} Hong
+      &copy; {new Date().getFullYear()} Nelson Lai
     </footer>
   )
 }
